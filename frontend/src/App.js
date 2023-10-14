b7f13dc43d9de04369b2f60cc3cdde5a845b9a60
Chandan <chandanharish.7006@gmail.com>
2023-10-14 18:15:11 +0530

    pagesincluded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,11 @@ import Login from "./pages/Login";
 import HomePage from "./pages/HomePage";
 import Kyc from "./pages/Kyc";
 import Register from "./pages/Register";
+import UserHome from "./pages/UserHome";
+import Lend from "./pages/Lend";
+import Borrow from './pages/Borrow';
+import Repay from "./pages/Repay";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -13,6 +18,11 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/kyc" element={<Kyc />} />
+          <Route path="/userhome" element={<UserHome />} />
+          <Route path="/lend" element={<Lend />} />
+          <Route path="/borrow" element={<Borrow />} />
+          <Route path="/repay" element={<Repay />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
   );
